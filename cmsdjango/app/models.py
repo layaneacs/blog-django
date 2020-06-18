b75aa23f13b17845f7cf8c5835927c801f2115e4
layane.andreia@hotmail.com <layane.andreia@hotmail.com>
2020-06-17 21:36:50 -0300

    criando o modelo post e add ao admin

--- cmsdjango/app/models.py
+++ cmsdjango/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+# models.Model = é um modelo do django
+class Post(models.Model):
+    # ForeignKey = chave estrangeira
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
+
